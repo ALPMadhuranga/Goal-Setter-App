3e419d0558952f626fcdd2c423e2c52f11e7c9fb
ALPMadhuranga <lahiruprasad520@gmail.com>
2024-02-26 15:51:14 +0530

    change path II

--- backend/server.js
+++ backend/server.js
@@ -24,13 +24,11 @@ app.use('/api/goals', goalRoutes);
 app.use('/api/users', userRoutes);
 
 // Serve frontend
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')));
-}else {
-    app.get('/', (req, res) => res.send('Please set to production'));
-}
+app.get("*", (req, res) => {
+  res.send(path.join(__dirname, "frontend", "build", "index.html"));
+});
 
 app.use(errorHandler);
 
